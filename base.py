4c0b4aff0f615e3a3c8d00d2318cbc8187e02d65
arazd <anastasia.razdaibiedina@mail.utoronto.ca>
2020-11-23 23:04:00 -0500

    update base

--- base.py
+++ base.py
@@ -10,30 +10,39 @@ from tensorflow.keras import Model
 class Saliency_Map():
     def __init__(self, model):
         """Constructs a Vanilla Gradient Map by computing dy/dx.
+
         Args:
         model: The TensorFlow model used to evaluate Gradient Map.
-        Should take image as input and output probabilities vector.
+            model takes image as input and outputs probabilities vector.
         """
         self.model = model
 
 
     def get_top_predicted_idx(self, img_processed):
+        """Outputs top predicted class for the input image.
+
+        Args:
+        img_processed: numpy image array in NHWC format, pre-processed according
+            to the defined model standard.
+
+        Returns:
+        Index of the top predicted class for the input image.
+        """
         preds = self.model.predict(img_processed)
         top_pred_idx = tf.argmax(preds[0])
         return top_pred_idx
-
 
 
     def get_gradients(self, image):
         """Computes the gradients of outputs w.r.t input image.
 
         Args:
-        img_input: 4D image tensor (NHWC)
+        image: numpy image array in NHWC format, pre-processed according
+            to the defined model standard.
 
         Returns:
-        Gradients of the predictions w.r.t img_input
+        Gradients of the predictions w.r.t image (same shape as input image)
         """
-        #images = tf.cast(self.image, tf.float32)
         top_pred_idx = self.get_top_predicted_idx(image)
 
         with tf.GradientTape() as tape:
@@ -45,10 +54,20 @@ class Saliency_Map():
         return grads
 
 
-    # normalize gradient to range between 0 and 1
     def norm_grad(self, grad_x):
-        abs_grads = tf.math.abs(grad_x)
+        """Normalizes gradient to the range between 0 and 1
+        (for visualization purposes).
+
+        Args:
+        grad_x: numpy gradients array.
+
+        Returns:
+        Gradients of the predictions w.r.t image (same shape as input image)
+        """
+        abs_grads = np.abs(grad_x)
         grad_max_ = np.max(abs_grads, axis=3)[0]
         arr_min, arr_max  = np.min(grad_max_), np.max(grad_max_)
         normalized_grad = (grad_max_ - arr_min) / (arr_max - arr_min + 1e-18)
-        return normalized_grad.reshape(1,grad_x.shape[1],grad_x.shape[2],1)
+        normalized_grad = normalized_grad.reshape(1,grad_x.shape[1],grad_x.shape[2],1)
+        
+        return normalized_grad
