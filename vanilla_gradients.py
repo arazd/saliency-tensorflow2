f3504a419f64ba0f9079f429feaadbe5cfc27d1b
arazd <anastasia.razdaibiedina@mail.utoronto.ca>
2020-11-29 20:38:37 -0500

    vanilla grads update

--- vanilla_gradients.py
+++ vanilla_gradients.py
@@ -4,15 +4,14 @@ import tensorflow as tf
 
 class VanillaGradients(SaliencyMap):
 
-    def get_mask(self, image, tensor_format=False):
+    def get_mask(self, image):
         """Constructs a Vanilla Gradient Map by computing dy/dx.
 
         Args:
             image: input image in NHWC format, not batched.
         """
         mask = self.get_gradients(image)
-        if not tensor_format:
-            mask = mask.numpy()
+
         return mask
 
 
